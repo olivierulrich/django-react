9baaf89444ce94ce9c6f4837cd8e50a1f9a4ff93
olivierulrich <olivier.ulrich@bluewin.ch>
2019-02-15 15:20:06 +0100

    comments

--- frontend/src/components/Testimonials.js
+++ frontend/src/components/Testimonials.js
@@ -1,9 +1,12 @@
 import React, {Component} from "react";
 import axios from "axios";
-import Heading from 'react-bulma-components/lib/components/heading';
 import Section from 'react-bulma-components/lib/components/section';
 
 class Testimonials extends Component {
+
+    /*
+    Constructor to set up the item attributes according to the API
+     */
     constructor(props) {
         super(props);
         this.state = {
@@ -21,6 +24,9 @@ class Testimonials extends Component {
         this.refreshList();
     }
 
+    /*
+    gets the data from the corresponding API endpoint, uses List defined in the constructor
+     */
     refreshList = () => {
         axios
             .get("/api/testimonial/")
@@ -28,6 +34,9 @@ class Testimonials extends Component {
             .catch(err => console.log(err));
     };
 
+    /*
+    maps the data from the API to the HTML element.
+     */
     renderItems = () => {
         return this.state.testimonialList.map(item => (
             <li>
