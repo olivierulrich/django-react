9c365e10f6b4c5d798ea7edff4cc279e00aa5570
olivierulrich <olivier.ulrich@bluewin.ch>
2019-02-18 13:41:52 +0100

    added mobile menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,28 +4,39 @@ import Container from 'react-bulma-components/lib/components/container';
 import TopNavbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Images from "./components/Images";
+import Loader from 'react-loading-spinner';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {isLoading: true}
+    }
+
+    componentDidMount() {
+        this.setState({isLoading: false})
+    }
 
     render() {
-        return (
-            <div>
-
-                <TopNavbar/>
-
-                <Images/>
-
-                <Container fluid>
 
 
-                    <Testimonials/>
+        return this.state.isLoading ? <Loader type="Puff" color="blue" height="100" width="100"/> : <div>
 
-                </Container>
 
-                <Footer/>
-            </div>
+            <TopNavbar/>
 
-        )
+            <Images/>
+
+            <Container fluid>
+
+
+                <Testimonials/>
+
+            </Container>
+
+            <Footer/>
+        </div>;
+
+
     }
 
 }
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,37 +2,56 @@ import React, {Component} from "react";
 import '../_variables.sass'
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import Navbar from 'react-bulma-components/lib/components/navbar';
-import Container from 'react-bulma-components/lib/components/container'
+import Container from 'react-bulma-components/lib/components/container';
+import Dropdown from 'react-bulma-components/lib/components/dropdown';
 import logo from "../logo.png";
 
 class TopNavbar extends Component {
+    state = {active: false};
+
+    handleClick = () => {
+        const {active} = this.state;
+        this.setState({active: !active});
+    };
+
     render() {
         return (
             <Navbar
                 fixed="top"
                 transparent="transparent"
+                active={this.state.active}
             >
                 <Container fluid>
-                <Navbar.Brand
-                >
-                    <Navbar.Item renderAs="a" href="#">
-                        <img
-                            src={logo}
-                            alt="Bunny's Buns"
-                            width="144"
+                    <Navbar.Brand
+                    >
+                        <Navbar.Item renderAs="a" href="#">
+                            <img
+                                src={logo}
+                                alt="Bunny's Buns"
+                                width="144"
+                            />
+                        </Navbar.Item>
+                        <Navbar.Burger
+                            active={this.state.active}
+                            onClick={this.handleClick}
                         />
-                    </Navbar.Item>
-                    <Navbar.Burger
-                    />
-                </Navbar.Brand>
-                <Navbar.Menu>
-                    <Navbar.Container>
-                        <Navbar.Item href="#" className="is-size-2">Bunny's Buns</Navbar.Item>
-                    </Navbar.Container>
-                    <Navbar.Container position="end">
-                        <Navbar.Item href="#">At the end</Navbar.Item>
-                    </Navbar.Container>
-                </Navbar.Menu>
+                    </Navbar.Brand>
+                    <Navbar.Menu>
+                        <Navbar.Container>
+                            <Navbar.Item href="#" className="is-size-2">Bunny's Buns</Navbar.Item>
+                        </Navbar.Container>
+                        <Navbar.Container position="end">
+                            <Navbar.Item href="#">
+                                Classes
+                            </Navbar.Item>
+                            <Navbar.Item href="#">
+                                About Us
+                            </Navbar.Item>
+                            <Navbar.Item href="#">
+                                Contact
+                            </Navbar.Item>
+                        </Navbar.Container>
+                    </Navbar.Menu>
                 </Container>
             </Navbar>
         );
